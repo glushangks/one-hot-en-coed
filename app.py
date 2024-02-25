c6f922e4f52c84fe8ffe4d0400f2a617f35e5cbb
GULSHAN RAJPUT <56313388+glushangks@users.noreply.github.com>
2024-02-25 10:15:45 +0530

    commit

---
+++ app.py
@@ -0,0 +1,33 @@
+# a=1023544356467576886987857867899898
+# #' ', " "    Single line 
+# # """ """     multi line
+# name='''komal hi welcome to python
+#  jhfugkhkhkjhgjfhgkhlh  
+#    jgkjkjhkjkjh  jhgjkjhk'''
+
+
+# c=20.5
+# d=False
+# print(type(a))
+# print(type(name))
+# print(type(c))
+# print(type(d))
+
+
+# a=10
+# b=20
+# c=a+b
+# print(c)
+# print(a+b)      #####   * ,/, -     #####   Intro  1) name 2) papa 3) address 4) phone 5) ps 6) pincode
+
+
+# ref & un-ref
+
+
+b=100
+c=1000
+a=b+c
+
+print(a)
+print(id(a))
+
